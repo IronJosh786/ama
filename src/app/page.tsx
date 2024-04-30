6cf637d86473c5c0743002721690788efcc6a72c
Faizan <sfaizan78692@gmail.com>
2024-04-30 11:49:31 +0530

    add send message button on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import {
   Carousel,
   CarouselContent,
@@ -45,6 +46,29 @@ const Layout = () => {
           ))}
         </CarouselContent>
       </Carousel>
+      <Link
+        id="send_btn"
+        href={"https://ama-opal.vercel.app/u/faizan"}
+        className="mt-4"
+      >
+        <div className="svg-wrapper-1">
+          <div className="svg-wrapper">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="16"
+              height="16"
+            >
+              <path fill="none" d="M0 0h24v24H0z"></path>
+              <path
+                fill="currentColor"
+                d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <span>Send</span>
+      </Link>
     </div>
   );
 };
